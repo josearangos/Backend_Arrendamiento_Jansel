a961627d301931c5a7d7cbbaa48fc4faf4fee7a4
Manilli <manillikun@gmail.com>
2018-04-28 13:47:54 -0500

    validacion de los campos del request y que no se encuentre vacio, tambien se añadio mas ciudades y prefijos de tipo de home

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
-    port:process.env.PORT || 4300,
+    port:process.env.PORT || 4800,
     dbMongo:process.env.MONGODB || 'mongodb://admin_Jansel:jansel@ds249249.mlab.com:49249/db_jansel_arrendamientos',
     prefixRoutes:'/v1'
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,55 @@
 
 'use strict' // convencio de EMC6
 const express = require('express');
+var   validator = require('../validations/validator');
 
 //controlador
 const homesCtrl= require('../controllers/homeController');
 const api=express.Router();
 
+
 api.post('/homes/search',function(req, res){
     
-    
-    var queryHomes= {
-        checkIn  : req.body.checkIn,
-        checkOut : req.body.checkOut,
-        city     : req.body.city,
-        type     : req.body.type
-    }    
-    homesCtrl.getHomes(queryHomes, function(err,data){
-            if (err==1) {
-                return res.status(500).send({ message: `Error al buscar ${data}` });
-            } else if(data==null) {
-                return res.status(404).send({ message: `El recurso solicitado no esta disponible` });
+    if(!validator.jsonIsEmpty(req.body)){  // validar que el request no sea vacio
+        
+        var failedParams = validator.paramsValidator(req.body);  // validar que el request tenga todos los campos de
+        var failedDates=validator.dateFormatValidation(req.body); // validar fechas checkin y checkout
+        var failedParamsType =validator.paramsType(req.body);
+
+        if(failedParams.length ==0){ // validar que tenga todos los campo
+                
+            if(failedParamsType.length ==0 ){ // validamos que tenga el tipo de dato correcto cada campo 
+
+                if(failedDates.length == 0){ // validamos el formato de las fechas
+
+                    var homesQuery= {
+                        checkIn  : req.body.checkIn,
+                        checkOut : req.body.checkOut,
+                        city     : req.body.city,
+                        type     : req.body.type
+                    }    
+                    homesCtrl.getHomes(homesQuery, function(err,data){
+                            if (err==1) {
+                                return res.status(500).send({ message: `Error al buscar ${data}` });
+                            } else if(data==null) {
+                                return res.status(404).send({ message: `El recurso solicitado no esta disponible` });
+                            }else{
+                                return res.status(200).send(data);
+                            }
+                    });
+                }else{
+                    return res.status(404).send({message: 'Fechas con formato incorrecto:'+failedDates});
+                }
             }else{
-                return res.status(200).send(data);
-            }
-    });
+                return res.status(404).send({message: 'Campos con tipo de dato incorrecto :'+failedParamsType}); 
+            }           
+        }else{
+            return res.status(404).send({message: 'Faltan los siguiete campos en el request:'+failedParams});
+        }        
+      
+    }else{
+        return res.status(404).send({message: 'El request esta vacio :/'});
+    }
 
 });
 
