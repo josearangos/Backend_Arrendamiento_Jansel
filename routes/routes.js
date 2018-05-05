fc61bc63a9854d0159b4ced8cec22c06ffb13016
jose.arangos <jose.arangos@udea.edu.co>
2018-05-05 10:11:30 -0500

    Hotfix: Not found status

--- routes/routes.js
+++ routes/routes.js
@@ -1,60 +1,60 @@
 
 'use strict' // convencio de EMC6
 const express = require('express');
-var   validator = require('../validations/validator');
+var validator = require('../validations/validator');
 
 //controlador
-const homesCtrl= require('../controllers/homeController');
-const api=express.Router();
+const homesCtrl = require('../controllers/homeController');
+const api = express.Router();
 
 
-api.post('/homes/search',function(req, res){
-    
-    if(!validator.jsonIsEmpty(req.body)){  // validar que el request no sea vacio
-        
+api.post('/homes/search', function (req, res) {
+
+    if (!validator.jsonIsEmpty(req.body)) {  // validar que el request no sea vacio
+
         var failedParams = validator.paramsValidator(req.body);  // validar que el request tenga todos los campos de
-        var failedDates=validator.dateFormatValidation(req.body); // validar fechas checkin y checkout
-        var failedParamsType =validator.paramsType(req.body);
+        var failedDates = validator.dateFormatValidation(req.body); // validar fechas checkin y checkout
+        var failedParamsType = validator.paramsType(req.body);
 
-        if(failedParams.length ==0){ // validar que tenga todos los campo
-                
-            if(failedParamsType.length ==0 ){ // validamos que tenga el tipo de dato correcto cada campo 
+        if (failedParams.length == 0) { // validar que tenga todos los campo
 
-                if(failedDates.length == 0){ // validamos el formato de las fechas
+            if (failedParamsType.length == 0) { // validamos que tenga el tipo de dato correcto cada campo 
 
-                    var homesQuery= {
-                        checkIn  : req.body.checkIn,
-                        checkOut : req.body.checkOut,
-                        city     : req.body.city,
-                        type     : req.body.type
-                    }    
-                    homesCtrl.getHomes(homesQuery, function(err,data){
-                            if (err==1) {
-                                return res.status(500).send({ message: `Error al buscar ${data}` });
-                            } else if(data==null) {
-                                return res.status(404).send({ message: `El recurso solicitado no esta disponible` });
-                            }else if(err == 2 || err == 3){
-                                return res.status(200).send(data);
-                            }else{
-                                return res.status(200).send(data);
-                            }
+                if (failedDates.length == 0) { // validamos el formato de las fechas
+
+                    var homesQuery = {
+                        checkIn: req.body.checkIn,
+                        checkOut: req.body.checkOut,
+                        city: req.body.city,
+                        type: req.body.type
+                    }
+                    homesCtrl.getHomes(homesQuery, function (err, data) {
+                        if (err == 1) {
+                            return res.status(500).send({ message: `Error al buscar ${data}` });
+                        } else if (data == null) {
+                            return res.status(200).send({ message: `El recurso solicitado no esta disponible` });
+                        } else if (err == 2 || err == 3) {
+                            return res.status(200).send(data);
+                        } else {
+                            return res.status(200).send(data);
+                        }
                     });
-                }else{
-                    return res.status(404).send({message: 'Fechas con formato incorrecto:'+failedDates});
+                } else {
+                    return res.status(404).send({ message: 'Fechas con formato incorrecto:' + failedDates });
                 }
-            }else{
-                return res.status(404).send({message: 'Campos con tipo de dato incorrecto :'+failedParamsType}); 
-            }           
-        }else{
-            return res.status(404).send({message: 'Faltan los siguiete campos en el request:'+failedParams});
-        }        
-      
-    }else{
-        return res.status(404).send({message: 'El request esta vacio :/'});
+            } else {
+                return res.status(404).send({ message: 'Campos con tipo de dato incorrecto :' + failedParamsType });
+            }
+        } else {
+            return res.status(404).send({ message: 'Faltan los siguiete campos en el request:' + failedParams });
+        }
+
+    } else {
+        return res.status(404).send({ message: 'El request esta vacio :/' });
     }
 
 });
 
 
 
-module.exports=api;
+module.exports = api;
