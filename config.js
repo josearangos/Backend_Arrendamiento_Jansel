2e1519ebbafed9229e7ac29ad01ec4b128bbaeef
josearangos <jose.arangos@udea.edu.co>
2018-05-31 17:06:10 -0500

    test automatico con codacy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
-    port:process.env.PORT || 4810,
+    port:process.env.PORT || 4610,
     dbMongo:process.env.MONGODB || 'mongodb://admin_Jansel:jansel@ds249249.mlab.com:49249/db_jansel_arrendamientos',
     prefixRoutes:'/v1'
 }
--- test/homeTest.js
+++ test/homeTest.js
@@ -16,7 +16,27 @@ describe('testUnit HomeController',function(){
          it('converts CO-CTG to Cartagena',function(){
             assert.equal(homeController.cityConverter('CO-CTG'),"Cartagena");          
          });
+
+         it('converts CO-MDE to Medellín',function(){
+            assert.equal(homeController.cityConverter('CO-MDE'),"Medellín");          
+         });
         
     });
 
+
+    describe('typeConverter codigo del tipo de casa',function(){
+        it('convierte de type 1 --> Apartamento ',function(){
+            assert.equal(homeController.typeConverter('1'),"Apartamento");        
+        });   
+     
+        it('convierte de type 2 --> Casa',function(){
+            assert.equal(homeController.typeConverter('2'),"Casa");          
+         });
+
+         it('convierte de type 3 --> Luxury',function(){
+            assert.equal(homeController.typeConverter('3'),"Luxury");          
+         });
+
+        
+    });
 });
