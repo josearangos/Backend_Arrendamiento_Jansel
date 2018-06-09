657e550fc27f9986a4ede7992a4bfc322b9d4bfb
Sebastian Osorno Zapata <osorno.sebastian@gmail.com>
2018-06-09 08:57:55 -0500

    Correciones menores

--- test/homeTest.js
+++ test/homeTest.js
@@ -331,30 +331,7 @@ describe('testUnit userController', function () {
 
 
 
-    describe('myBookings consulta las reservas de cada usuario segun su iud', function () {
-
-        
-
-        it('Retorna al validar con el uid = 9Mkgz46wmQX0nNSKqucrtkNaYJp1, no valido --> 1 ', function () {
-
-            userController.myBookings(
-                {
-                    "uid": "9Mkgz46wmQX0nNSKqucrtkNaYJp1"
-                },
-                {
-                    "agency":
-                        {
-                            "name": "Arrendamientos Santa Fé",
-                            "nit": "1123-1233-12313-51414"
-                        }
-                }, function (err, data) {
-                    assert.equal(err, 1);
-                })
-
-        });
-
-
-    });
+   
 
     describe('removeBooking consulta y cancela una reserva de un usuario segun su iud y bookingId ', function () {
 
@@ -545,6 +522,32 @@ describe('myBookings consulta las reservas de cada usuario segun su iud', functi
     });
 
   
+
+
+});
+
+
+describe('myBookings consulta las reservas de cada usuario segun su iud', function () {
+
+        
+
+    it('Retorna al validar con el uid = 9Mkgz46wmQX0nNSKqucrtkNaYJp1, no valido --> 1 ', function () {
+
+        userController.myBookings(
+            {
+                "uid": "9Mkgz46wmQX0nNSKqucrtkNaYJp1"
+            },
+            {
+                "agency":
+                    {
+                        "name": "Arrendamientos Santa Fé",
+                        "nit": "1123-1233-12313-51414"
+                    }
+            }, function (err, data) {
+                assert.equal(err, 1);
+            })
+
+    });
 
 
 });
