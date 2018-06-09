eaffe1b01e24250d4e3ec86e6743bce8488c11c2
Sebastian Osorno Zapata <osorno.sebastian@gmail.com>
2018-06-09 09:02:35 -0500

    Correciones menores

--- test/homeTest.js
+++ test/homeTest.js
@@ -335,50 +335,7 @@ describe('testUnit userController', function () {
 
     describe('removeBooking consulta y cancela una reserva de un usuario segun su iud y bookingId ', function () {
 
-        it('Retorna al validar con el uid (no valido) y bookingId cualquiera --> 1 ', function () {
-
-            userController.removeBooking(
-                {
-                    "uid": "9Mkgz46wmQX0nNSKqucrtkNaYJp1"
-                },
-
-                "1*07-04-2018*10-04-2018"
-
-                ,
-                {
-                    "agency":
-                        {
-                            "name": "Arrendamientos Santa Fé",
-                            "nit": "1123-1233-12313-51414"
-                        }
-                }, function (err, data) {
-                    assert.equal(err, 1);
-                })
-
-        });
-
-        it('Retorna al validar con el uid (no valido) y bookingId cualquiera --> 1 ', function () {
-
-            userController.removeBooking(
-                {
-                    "uid": "9Mkgz46wmQX0nNSKqucrtkNaYJp1"
-                },
-
-                "1*07-04-2018*10-04-2018"
-
-                ,
-                {
-                    "agency":
-                        {
-                            "name": "Arrendamientos Santa Fé",
-                            "nit": "1123-1233-12313-51414"
-                        }
-                }, function (err, data) {
-                    assert.equal(err, 1);
-                })
-
-        });
-
+        
 
         it('Retorna al consultar en las fechas "checkIn": "07-06-2018"-"checkOut": "10-06-2018", --> [0, Mensaje OK]', function () {
 
@@ -548,6 +505,63 @@ describe('myBookings consulta las reservas de cada usuario segun su iud', functi
             })
 
     });
+
+
+});
+
+
+describe('removeBooking consulta y cancela una reserva de un usuario segun su iud y bookingId ', function () {
+    it('Retorna al validar con el uid (no valido) y bookingId cualquiera --> 1 ', function () {
+
+        userController.removeBooking(
+            {
+                "uid": "9Mkgz46wmQX0nNSKqucrtkNaYJp1"
+            },
+
+            "1*07-04-2018*10-04-2018"
+
+            ,
+            {
+                "agency":
+                    {
+                        "name": "Arrendamientos Santa Fé",
+                        "nit": "1123-1233-12313-51414"
+                    }
+            }, function (err, data) {
+                assert.equal(err, 1);
+            })
+
+    });
+
+
+
+});
+
+describe('removeBooking consulta y cancela una reserva de un usuario segun su iud y bookingId ', function () {
+    it('Retorna al validar con el uid (no valido) y bookingId cualquiera --> 1 ', function () {
+
+        userController.removeBooking(
+            {
+                "uid": "9Mkgz46wmQX0nNSKqucrtkNaYJp1"
+            },
+
+            "1*07-04-2018*10-04-2018"
+
+            ,
+            {
+                "agency":
+                    {
+                        "name": "Arrendamientos Santa Fé",
+                        "nit": "1123-1233-12313-51414"
+                    }
+            }, function (err, data) {
+                assert.equal(err, 1);
+            })
+
+    });
+
+
+
 
 
 });
